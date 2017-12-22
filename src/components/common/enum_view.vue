aaaa46278918dedb0cdf01b37c8b5cb3c251c997
jiangshanmeta <540118044@qq.com>
2017-12-22 20:17:08 +0800

    common component enum_view

---
+++ src/components/common/enum_view.vue
@@ -0,0 +1,19 @@
+<template>
+    <span>{{enums[data]}}</span>
+</template>
+
+<script>
+export default{
+    name:"enum_view",
+    props:{
+        data:{
+            type:[Number,String],
+            required:true
+        },
+        enums:{
+            type:Object,
+            required:true,
+        }
+    },
+}
+</script>
