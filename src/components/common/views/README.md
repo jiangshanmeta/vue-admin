25de4a1c8df9ce9d578657bece552ef7c05bfe1b
jiangshanmeta <540118044@qq.com>
2018-01-15 20:57:26 +0800

    view_staticpath

--- src/components/common/views/README.md
+++ src/components/common/views/README.md
@@ -9,3 +9,10 @@
 | data   | 是 | String Number | 真实值 | 作为showComponent时该值自动传入   |
 | enums  |  是  | Object    | 枚举配置项 | - |
 
+## view_staticpath
+
+设计之初设为了展示注册在store上的内容
+
+| 属性名 | 是否必需  | 类型      | 属性描述 |  备注 |
+| :---:  | :--:  | :--: | :-----:  | :--: |
+| path   | 是    | String | 路径描述 | 例如 "$store.state.activity.name" |
---
+++ src/components/common/views/view_staticpath.vue
@@ -0,0 +1,28 @@
+<template>
+    <span>{{pathValue}}</span>
+</template>
+
+<script>
+export default{
+    name:"view_staticpath",
+    props:{
+        path:{
+            type:String,
+            default:""
+        }
+    },
+    computed:{
+        pathValue(){
+            let arr = this.path.split(".");
+            if(!arr.length){
+                return "";
+            }
+
+            return arr.reduce((pre,key)=>{
+                return pre[key];
+            },this)
+
+        }
+    },
+}
+</script>
