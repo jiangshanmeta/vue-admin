59203b45506de492c110ad03fd3e564aee8cc108
jiangshanmeta <540118044@qq.com>
2018-02-17 10:00:18 +0800

    fix bug for vue-config in mixin

--- src/widget/vue-config.js
+++ src/widget/vue-config.js
@@ -1,9 +1,11 @@
 export default {
     install(Vue){
+        // mixin的时候会用到
+        Vue.config.optionMergeStrategies.config = Vue.config.optionMergeStrategies.computed;
+
         Vue.mixin({
             beforeCreate:function(){
-                const options = this.$options;
-                const config = options.config;
+                const config = this.$options.config;
                 if(config && typeof config === 'object'){
                     let keys = Object.keys(config);
                     keys.forEach((key)=>{
