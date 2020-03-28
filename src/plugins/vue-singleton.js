36b6e962e9212a8d6e8aa21e8ee4abfe06683b73
jiangshanmeta <540118044@qq.com>
2020-03-29 00:59:17 +0800

    vue-singleton $singleton直接挂原型上

--- src/plugins/vue-singleton.js
+++ src/plugins/vue-singleton.js
@@ -1,5 +1,34 @@
 export default {
     install (Vue) {
+        Vue.prototype.$singleton = function (Component, propsData = {}, dependent) {
+            Component = Vue.extend(Component);
+            if (!Component.instance) {
+                Component.instance = new Component({
+                    // 处理store
+                    store: this.$store,
+                    propsData,
+                });
+
+                Component.dependent = [];
+                Component.instance.$mount();
+                document.body.appendChild(Component.instance.$el);
+            } else {
+                Object.assign(Component.instance, propsData);
+            }
+
+            if (dependent instanceof Vue && !Component.dependent.includes(dependent)) {
+                Component.dependent.push(dependent);
+                if (!Array.isArray(dependent._relateSingleton)) {
+                    dependent._relateSingleton = [];
+                }
+                if (!dependent._relateSingleton.includes(Component)) {
+                    dependent._relateSingleton.push(Component);
+                }
+            }
+
+            return Component.instance;
+        };
+
         Vue.mixin({
             beforeDestroy () {
                 if (!Array.isArray(this._relateSingleton)) {
@@ -14,38 +43,6 @@ export default {
                     }
                 });
             },
-            methods: {
-                $singleton (Component, propsData = {}, dependent) {
-                    Component = Vue.extend(Component);
-                    if (!Component.instance) {
-                        Component.instance = new Component({
-                            // 处理store
-                            store: this.$store,
-                            propsData,
-                        });
-
-                        Component.dependent = [];
-                        Component.instance.$mount();
-                        document.body.appendChild(Component.instance.$el);
-                    } else {
-                        Object.assign(Component.instance, propsData);
-                    }
-
-                    if (dependent instanceof Vue && !Component.dependent.includes(dependent)) {
-                        Component.dependent.push(dependent);
-                        if (!Array.isArray(dependent._relateSingleton)) {
-                            dependent._relateSingleton = [];
-                        }
-                        if (!dependent._relateSingleton.includes(Component)) {
-                            dependent._relateSingleton.push(Component);
-                        }
-                    }
-
-                    return Component.instance;
-                },
-            },
-
         });
     },
-}
-;
+};
